872abadbe0576400c04e64b0d543ad1425b2ea29
Giulio Cocconi <coccogiulio8@gmail.com>
2025-04-22 11:02:32 +0200

    Wire logic

--- src/ui/common/graphicalWire.hpp
+++ src/ui/common/graphicalWire.hpp
@@ -1,27 +1,87 @@
+/*
+  Copyright (C) 2025 Giulio Cocconi
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 #include <cassert>
+#include <ranges>
 #include <vector>
 
 #include <QGraphicsItem>
+#include <QPainter>
 #include <QPainterPath>
 #include <QPoint>
 
 #include <core/wire.hpp>
-#include <ui/common/diagramView.hpp>
+
+class GraphicalWire;
+
+class GraphicalWireJunction : public QGraphicsItem {
+public:
+  GraphicalWireJunction(QPoint point, QGraphicsItem* parent);
+  void paint(QPainter* painter, const QStyleOptionGraphicsItem* option,
+             QWidget* widget) override;
+
+private:
+  QRectF boundingRect() const override;
+
+  static const int RADIUS = 5;
+
+  QPoint point;
+};
+
+class GraphicalWireSegment : public QGraphicsItem {
+public:
+  GraphicalWireSegment(QPointF firstPoint, QGraphicsItem* parent = nullptr);
+  void         paint(QPainter* painter, const QStyleOptionGraphicsItem* option,
+                     QWidget* widget) override;
+  QPainterPath shape() const override { return this->path; }
+
+  bool isCompleted() { return false; };
+  bool isPointOnPath(const QPointF point);
+
+  void addPoints();
+
+  void                 setShowPoints(std::vector<QPointF> showPoints);
+  std::vector<QPointF> getShowPoints() { return showPoints; }
+
+  QPointF lastPoint() { return points[points.size() - 1]; }
+
+private:
+  QPainterPath path;
+
+  std::vector<QPointF> points;
+  std::vector<QPointF> showPoints;
+  GraphicalWire*       graphicalWire = nullptr;
+
+  QRectF boundingRect() const override;
+
+  void updatePath();
+};
 
 class GraphicalWire : public QGraphicsItem {
 public:
-  GraphicalWire(std::vector<QPoint> points, QGraphicsItem* parent = nullptr);
-  GraphicalWire(QPainterPath path, Bus bus, QGraphicsItem* parent = nullptr);
-  void updatePoints(std::vector<QPoint> points);
-  void addPoint(QPoint point);
+  GraphicalWire(QGraphicsItem* parent = nullptr);
+  GraphicalWire(std::vector<GraphicalWireSegment> segments,
+                QGraphicsItem*                    parent = nullptr);
 
   void setBus(Bus bus);
   Bus  getBus();
 
 private:
-  QPainterPath path;
-
   Bus bus;
 };
